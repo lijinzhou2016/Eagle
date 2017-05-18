8df4276a75f4ba3d9bf874d593d81389a8003b16
archermind <yu.wang@archermind.com>
2017-05-18 16:23:33 +0800

    ubuntu同时开始多个终端执行脚本测试成功

--- do.py
+++ do.py
@@ -17,7 +17,6 @@ print sys.argv[1]
 print sys.argv[2]
 print sys.argv[3]
 print sys.argv[4]
-print sys.argv[5]
 
 time.sleep(5)
 
--- start.py
+++ start.py
@@ -17,15 +17,15 @@ with open('log.path', mode='w') as f:
     f.write(log_absoulte_root_path)
 
 
-start_cmd = "gnome-terminal --maximize"
+start_cmd = 'gnome-terminal --maximize'
 script = os.path.join(settings.BASE_DIR, 'do.py')
 for serialno, loop in settings.DEVICES:
-    start_cmd += " --tab -t '{0}' ".format(serialno) \
-                 + "-e python {0} {1} {2} {3} {4} {5}".format(
-        script, serialno, loop, settings.CASE_CONF_LIST_PATH, settings.CASE_SOURCE_PATH, log_absoulte_root_path)
+    start_cmd += ' --tab -t "{0}" '.format(serialno) \
+                 + '-e "python {0} {1} {2} {3} {4} {5}"'.format(
+            script, serialno, loop, settings.CASE_CONF_LIST_PATH, settings.CASE_SOURCE_PATH, log_absoulte_root_path)
 
 
 
 print start_cmd
 
-# os.system(start_cmd)
+os.system(start_cmd)
