d5e763437fb2f50fe78a7106fade89acb0a6af94
Jinzhou.li <414820128@qq.com>
2017-06-01 13:44:42 +0800

    测试蓝牙配对脚本

---
+++ pandaMTBF/bluetooth.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import os
+import sys
+import unittest
+import time
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from common import frame
+
+
+# 主设备待测app包名和activity
+# 必填
+appPackage = 'com.android.settings'
+appActivity = ".Settings"
+
+# 辅助设备待测app包名和activity
+# 若无辅助设备，可以为空
+sappPackage = 'com.android.settings'
+sappActivity = ".Settings"
+
+os.environ.setdefault("appPackage", appPackage)
+os.environ.setdefault("appActivity", appActivity)
+os.environ.setdefault("sappPackage", sappPackage)
+os.environ.setdefault("sappActivity", sappActivity)
+
+
+class Bluetooth(frame.DecorateDriver):
+
+    def __init__(self, driver, logger):
+        self.logger = logger
+        self.driver = driver
+
+    def enter_bt_view(self):
+        pass
+
+
+
+
+
+
+class AndroidTestCases(frame.BaseCase):
+
+    def setUp(self):
+        self.driver, self.s_driver = self.init()
+        self.m_bt = Bluetooth(self.driver, self.logger)
+        if self.s_driver:
+            self.s_bt = Bluetooth(self.s_driver, self.logger)
+
+
+    def tearDown(self):
+        self.clear()
+
+
+    def test_connected_and_disconnected(self):
+        """
+        蓝牙配对与取消
+        :return:
+        """
+        self.logger.debug("enter bluetooth view")
+        self.driver.find_element_by_android_uiautomator('new UiSelector().text("蓝牙")').click()
+        time.sleep(5)
+        self.driver.find_element_by_android_uiautomator('new UiSelector().text("ic")').click()
+        time.sleep(5)
+        self.s_driver.find_element_by_android_uiautomator('new UiSelector().text("配对")').click()
+        time.sleep(2)
+
+
+        self.result.success()
+
+
+
+    # def test_connected_and_send_picture(self):
+    #     """
+    #     蓝牙配对，主设备发送一张图片
+    #     :return:
+    #     """
+    #     pass
+
+
--- pandaMTBF/common/appium_server.py
+++ pandaMTBF/common/appium_server.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+"""
+脚本编写调试阶段，启用此服务
+"""
 
 __author__ = "LiJinzhou"
 __date__ = "2017/5/15 下午2:26"
@@ -16,24 +19,18 @@ platform = sys.platform
 def sub(cmd):
     print cmd
     subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True).wait()
-#
-# def win_sbu(cmd):
-#     print cmd
-#     os.system(cmd)
 
 
-def mac_server_start():
+def server_start(cmd):
     thread = []
-    m_cmd = "node " \
-            + "/Applications/Appium.app/Contents/Resources/node_modules/appium/build/lib/main.js" \
+    m_cmd = cmd \
             + " --port " + port \
             + " --udid " + deviceName \
             + " --address 127.0.0.1"
     thread.append(threading.Thread(target=sub, args=(m_cmd,)))
 
     if assistant == "true":
-        s_cmd = "node " \
-                + "/Applications/Appium.app/Contents/Resources/node_modules/appium/build/lib/main.js" \
+        s_cmd = cmd \
                 + " --port " + sport \
                 + " --udid " + sdeviceName \
                 + " --address 127.0.0.1"
@@ -44,67 +41,31 @@ def mac_server_start():
     for t in thread:
         t.join()
 
-
-def win_server_start():
-    thread = []
-    m_cmd = "appium " \
-            + " --port " + port \
-            + " --udid " + deviceName \
-            + " --address 127.0.0.1"
-    thread.append(threading.Thread(target=sub, args=(m_cmd,)))
-
-    if assistant == "true":
-        s_cmd = "appium " \
-                + " --port " + sport \
-                + " --udid " + sdeviceName \
-                + " --address 127.0.0.1"
-        thread.append(threading.Thread(target=sub, args=(s_cmd,)))
-
-    for t in thread:
-        t.start()
-    for t in thread:
-        t.join()
-
-def linux_sercer_start():
-    thread = []
-    m_cmd = "node " \
-            + "/Applications/Appium.app/Contents/Resources/node_modules/appium/build/lib/main.js" \
-            + " --port " + port \
-            + " --udid " + deviceName \
-            + " --address 127.0.0.1"
-    thread.append(threading.Thread(target=sub, args=(m_cmd,)))
-
-    if assistant == "true":
-        s_cmd = "node " \
-                + "/Applications/Appium.app/Contents/Resources/node_modules/appium/build/lib/main.js" \
-                + " --port " + sport \
-                + " --udid " + sdeviceName \
-                + " --address 127.0.0.1"
-        thread.append(threading.Thread(target=sub, args=(s_cmd,)))
-
-    for t in thread:
-        t.start()
-    for t in thread:
-        t.join()
 
 with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "config", "devicesinfo.json")) as json_file:
     json_data = json.load(json_file)
 
 assistant = json_data["info"]["assistant"]
+
 deviceName = json_data["mdevice"]["deviceName"]
 port = json_data["mdevice"]["port"]
 
-if assistant == "true":
-    sdeviceName = json_data["sdevice"]["sdeviceName"]
-    sport = json_data["sdevice"]["sport"]
+sdeviceName = json_data["sdevice"]["sdeviceName"]
+sport = json_data["sdevice"]["sport"]
+
+win_appium_path = json_data["info"]["win_appium_path"]
+mac_appium_path = json_data["info"]["mac_appium_path"]
+
 
 if platform == "darwin":
-    mac_server_start()
+    cmd = "node " + mac_appium_path
+    server_start(cmd)
 
 elif "win32" in platform:
-    win_server_start()
+    cmd = "node " + win_appium_path
+    server_start(cmd)
 
 elif "linux" in platform:
-    linux_sercer_start()
+    server_start("appium")
 
 
